af04c6b4764bc4375f70f6086225878b04dad79b
Manuel <manuelheller@bluewin.ch>
2021-12-03 17:30:43 +0100

    test

--- assets/php/dbh.inc.php
+++ assets/php/dbh.inc.php
@@ -1,8 +1,17 @@
 <?php
 
+// ci cd
+
 $dbServername = "localhost";
 $dbUsername = "root";
 $dbPassword = "";
 $dbName = "mazemaster";
+
+/*
+$dbServername = "rushpupp.mysql.db.internal";
+$dbUsername = "rushpupp_manu";
+$dbPassword = "FZoFhaDQ+ZNFKP8JtW*H";
+$dbName = "rushpupp_manu";
+*/
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
